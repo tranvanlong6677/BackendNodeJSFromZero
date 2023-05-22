5250e7f55713609a895568b09d124f06c2a5f21d
“Tran <tranvanlong6677@gmail.com>
2023-05-22 11:16:55 +0700

    config .env

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { hostname } from "os";
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOSTNAME;
 
-app.set("views",path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -21,6 +24,6 @@ app.get("/abcd", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
