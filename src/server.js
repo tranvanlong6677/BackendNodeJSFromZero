07534b9355d01970b87192923f8a53d801f3f921
“Tran <tranvanlong6677@gmail.com>
2023-05-22 13:21:27 +0700

    config nodemon and static file

--- src/server.js
+++ src/server.js
@@ -2,23 +2,25 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { hostname } from "os";
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8080;
-const hostname = process.env.HOSTNAME;
+const hostname = process.env.HOST_NAME || "localhost";
+
+app.use(express.static(path.join(__dirname,'public')))
+console.log(">>> check dirname",__dirname);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!111");
 });
 app.get("/abc", (req, res) => {
-  res.send("abc check");
+  res.send("abc check11");
 });
 app.get("/abcd", (req, res) => {
   res.render("sample.ejs");
