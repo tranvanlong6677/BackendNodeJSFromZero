b268f63b7dc37fd92a90f4dfe7777ba5fc20cb7d
“Tran <tranvanlong6677@gmail.com>
2023-05-23 09:56:56 +0700

    design list user and call api getalluser

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,23 +1,49 @@
 import connection from "../config/database.js";
 
-const getHomePage = (req, res) => {
-    return res.render("home.ejs");
-    
+const getHomePage = async (req, res) => {
+  const [rows, fields] = await connection.execute(
+    "SELECT * FROM Users",
+  );
+  console.log(">>> check rows",rows);
+  return res.render("home.ejs");
 };
 
 const getSample = (req, res) => {
   res.render("sample.ejs");
 };
 
-const postCreateUser = (req, res) => {
-  console.log(">>> check req body",req.body);
+const postCreateUser = async (req, res) => {
+  console.log(">>> check req body", req.body);
   const { email, name, city } = req.body;
-  connection.query(
-    `INSERT INTO Users (email,name,city) VALUES (?,?,?)`,[email, name, city],
-    function(err, results, fields) {
-      console.log(results); 
-      res.send("Create user success");
-    }
+
+  // connection.query(
+  //   `INSERT INTO Users (email,name,city) VALUES (?,?,?)`,
+  //   [email, name, city],
+  //   function (err, results, fields) {
+  //     console.log(results);
+  //     res.send("Create user success");
+  //   }
+  // );
+  const [rows, fields] = await connection.execute(
+    "INSERT INTO Users (email,name,city) VALUES (?,?,?)",
+    [email, name, city]
   );
-}
-export { getHomePage, getSample,postCreateUser };
+  
+  res.send("Create user success");
+};
+
+const getCreatePage = (req, res) => {
+  res.render("create.ejs");
+};
+
+const getAllUser = async(req, res) => {
+  // connection.query(`SELECT * FROM Users`, function (err, results, fields) {
+  //   console.log(results);
+  // });
+  const [rows, fields] = await connection.execute(
+    "SELECT * FROM Users",
+  );
+  console.log(">>> check rows",rows);
+  console.log(">>> check fields",fields);
+};
+export { getHomePage, getSample, postCreateUser, getCreatePage, getAllUser };
--- src/routes/web.js
+++ src/routes/web.js
@@ -2,13 +2,16 @@ import express from "express";
 import {
   getHomePage,
   getSample,
+  getCreatePage,
   postCreateUser,
+  getAllUser,
 } from "../controllers/homeControllers.js";
 
 const router = express.Router();
 
 router.get("/", getHomePage);
 router.post("/create-user", postCreateUser);
+router.get("/create",getCreatePage);
 router.get("/sample", getSample);
 
 export default router;
