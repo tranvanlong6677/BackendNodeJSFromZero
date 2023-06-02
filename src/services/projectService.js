3bb3fa73e336311432dde9d564d4cdbd87db12cf
long <tranvanlong6677@gmail.com>
2023-06-02 13:58:59 +0700

    add user to usersInfor project

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -6,6 +6,21 @@ const postProjectService = async (infoProject) => {
     if (infoProject.type === "EMPTY_PROJECT") {
       data = await Project.create({ ...infoProject });
     }
+    if (infoProject.type === "ADD_USER") {
+      let project = await Project.findById(infoProject.projectId);
+      console.log(">>> check project", project);
+      console.log(">>> check infoProject", infoProject);
+
+      for (let i = 0; i < infoProject.usersArr.length; i++) {
+        // infoProject
+        if (!project.usersInfor.includes(infoProject.usersArr[i])) {
+          project.usersInfor.push(infoProject.usersArr[i]);
+        }
+      }
+      let newProject = await project.save();
+      console.log(">>> check new project", newProject);
+      return newProject;
+    }
   } catch (error) {
     console.log(error);
     data = null;
